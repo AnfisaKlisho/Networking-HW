37d09e4d646f20fc91f7ad6b5e58a18857bd9190
Anfisa Klisho <klisho.anfisa@yandex.ru>
2020-11-25 14:19:01 +0300

    first version

---
+++ Networking HW/SWPlanet.swift
@@ -0,0 +1,16 @@
+//
+//  SWPlanet.swift
+//  Networking HW
+//
+//  Created by Anfisa Klisho on 25.11.2020.
+//
+
+import Foundation
+
+struct SWPlanet{
+    var name: String
+    var population: String
+    var diameter: Int
+    var climate: String
+}
+
